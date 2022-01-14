4a99d4e8b89a4398c464d6a40bb79929542bdcad
irassl <smith.ya-s@yandex.ru>
2022-01-14 16:26:49 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react'
+import { Transition } from 'react-transition-group';
+
+
+
+export default function App() {
+  const [toggle, setToggle] = useState(true)
+
+  return (
+    <div className="container">
+      <button onClick={() => setToggle(!toggle)}>Toggle</button>
+      <hr />
+      <div className={'blocks'}>
+        <Transition
+          in={toggle}
+          timeout={{
+            enter: 1000,
+            exit: 500
+          }}
+          mountOnEnter
+          unmountOnExit
+        >
+          {state => <div className={`square blue ${state}`}>{state}</div>}
+
+        </Transition>
+      </div>
+
+    </div>
+  );
+}
+
